52bd53760a4e039982795e766fcc3cf726966ddb
Predrag <Predrag@fujitsuserver>
2017-06-16 15:07:13 +0200

    Test

--- src/fuel_injection_perfection/Answer.java
+++ src/fuel_injection_perfection/Answer.java
@@ -17,36 +17,46 @@ public class Answer {
         // Your code goes here.
         ArrayList<Long> dividedString = new ArrayList();
         
-        int len = 3;
+        int len = 2;
         
         int i = n.length() / len;
         int j = n.length() % len;
-
-        dividedString.add(Long.parseLong(n.substring(0, j)));
+        if (j > 0) 
+            dividedString.add(Long.parseLong(n.substring(0, j)));
         for (int k = 0; k < i; k++) {
             dividedString.add(Long.parseLong(n.substring(j + k * len, j + (k + 1) * len)));
         }
 
         int size = dividedString.size();
-        
-        while (size > 1) {
+        int oprtation= 0;
+        while (size > 0) {
             if (dividedString.get(size - 1) % 2 == 1) {
                 dividedString.set(size - 1, dividedString.get(size - 1) + 1);
+                oprtation++;
+                continue;
             } else {
-                for (int k = 0; k < 10; k++) {
-                
+                for (int k = 0; k < size; k++) {
+                    long ii = dividedString.get(k) % 2;
+                    if (ii == 1) {
+                        long jjj = (long)(Math.pow(10, len));
+                        dividedString.set(k+1, dividedString.get(k+1) + jjj);
+                    }                                        
+                    long jj = dividedString.get(k) / 2;
+                    dividedString.set(k, jj);
                 }
             }
-            
-        }
-        
-        while (dividedString.get(0) > 1) {
-            
+            if (size > 1 && dividedString.get(0) == 0) {
+                dividedString.remove(0);
+                size--;
+            } else if (size == 1 && dividedString.get(0) == 1) {
+                size--;
+            }
+            oprtation++;
         }
 
-        return 0;
+        return oprtation;
     }
     public static void main(String[] args) {
-        answer("12123123123123123");
+        System.out.println(answer("100000"));
     }
 }
